38a19f7929bab4fd2eb13482b0c3cc1ab7a67b6b
Juan <jidrovo2019@fau.edu>
2023-05-23 16:06:45 -0500

    fixing bug 4.0

--- DataAnalytics/streamlit/pages/05_Test.py
+++ DataAnalytics/streamlit/pages/05_Test.py
@@ -86,7 +86,7 @@ st.markdown('<p class = "title_3">Filter by Year:</p>', unsafe_allow_html=True)
 min_year = int(df['time'].dt.year.min())
 max_year = int(df['time'].dt.year.max())
 
-selected_year = st.slider("  ", min_value=min_year, max_value=max_year)
+selected_year = st.slider("  ", min_value=min_year, max_value=(max_year - 1))
 
 # Filter the data based on the selected year
 filtered_df = df[df['time'].dt.year == selected_year]
